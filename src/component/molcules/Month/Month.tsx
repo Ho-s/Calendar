712d963e959029b605398f4c4e38ccb901701416
Ho-s <kjcoco13@gmail.com>
2021-09-07 16:00:57 +0900

    New eslint style

--- src/component/molcules/Month/Month.tsx
+++ src/component/molcules/Month/Month.tsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import moment from 'moment';
-import './style.css';
+import React, {useState} from 'react'
+import moment from 'moment'
+import './style.css'
 
 interface IMonth {
     nowDay:number
@@ -14,116 +14,116 @@ interface IMonth {
 }
 
 const Month = ({nowDay,
-  nowWeek,
-  monthStorage,
-  yearStorage,
-  nowMonth,
-  nowYear,
-  onClickDayInMonth,
-  storage}:IMonth) => {
-  const m = moment();
-  const [today, setToday] = useState(m.format('YYYYMMDD'));
+	nowWeek,
+	monthStorage,
+	yearStorage,
+	nowMonth,
+	nowYear,
+	onClickDayInMonth,
+	storage}:IMonth) => {
+	const m = moment()
+	const [today, setToday] = useState(m.format('YYYYMMDD'))
 
-  const Generate = () => {
-    m.set('year', nowYear);
-    m.set('month', nowMonth);
-    const startWeek = m.clone().startOf('month').week();
-    const endWeek = m.clone().endOf('month').week() === 1 ? 53 : m.clone().endOf('month').week();
-    const calendar = [];
-    for (let week = startWeek; week <= endWeek; week++) {
-      calendar.push(
-          <div className="month-row" key={week}>
-            {
-              Array(7).fill(0).map((n, i) => {
-                const current = m.clone().week(week).startOf('week').add(n + i, 'day');
-                const todaySelected = today === current.format('YYYYMMDD') ? 'month-selected' : '';
-                const isSelected = (Number(nowDay) === Number(current.format('D')) && Number(yearStorage) === nowYear && monthStorage === nowMonth) ? 'monthSelected' : '';
-                const isGrayed = current.format('MM') === m.format('MM') ? '' : 'month-grayed';
-                const clicking = current.format('MM') === m.format('MM') ? onClickDayInMonth : ((e: any) => e.preventDefault());
+	const Generate = () => {
+		m.set('year', nowYear)
+		m.set('month', nowMonth)
+		const startWeek = m.clone().startOf('month').week()
+		const endWeek = m.clone().endOf('month').week() === 1 ? 53 : m.clone().endOf('month').week()
+		const calendar = []
+		for (let week = startWeek; week <= endWeek; week++) {
+			calendar.push(
+				<div className="month-row" key={week}>
+					{
+						Array(7).fill(0).map((n, i) => {
+							const current = m.clone().week(week).startOf('week').add(n + i, 'day')
+							const todaySelected = today === current.format('YYYYMMDD') ? 'month-selected' : ''
+							const isSelected = (Number(nowDay) === Number(current.format('D')) && Number(yearStorage) === nowYear && monthStorage === nowMonth) ? 'monthSelected' : ''
+							const isGrayed = current.format('MM') === m.format('MM') ? '' : 'month-grayed'
+							const clicking = current.format('MM') === m.format('MM') ? onClickDayInMonth : ((e: any) => e.preventDefault())
 
-                const day: any = [];
-                if (current.format('MM') === m.format('MM')) {
-                  {
-                    Array(storage.length).forEach((v, n) => {
-                      if (storage[n].year === Number(current.year())) {
-                        if (storage[n].month === Number(current.month() + 1)) {
-                          if (storage[n].day === Number(current.date())) {
-                            day.push(storage[n]);
-                          }
-                        }
-                      }
-                    });
-                  }
-                }
+							const day: any = []
+							if (current.format('MM') === m.format('MM')) {
+								{
+									Array(storage.length).forEach((v, n) => {
+										if (storage[n].year === Number(current.year())) {
+											if (storage[n].month === Number(current.month() + 1)) {
+												if (storage[n].day === Number(current.date())) {
+													day.push(storage[n])
+												}
+											}
+										}
+									})
+								}
+							}
 
-                // for sorting
-                function compare(a:any, b:any) {
-                  const A = Number(a.startHours + a.startMinutes);
-                  const B = Number(b.startHours + a.startMinutes);
+							// for sorting
+							function compare(a:any, b:any) {
+								const A = Number(a.startHours + a.startMinutes)
+								const B = Number(b.startHours + a.startMinutes)
 
-                  let comparison = 0;
-                  if (A > B) {
-                    comparison = 1;
-                  } else if (A < B) {
-                    comparison = -1;
-                  }
-                  return comparison;
-                }
-                day.sort(compare);
+								let comparison = 0
+								if (A > B) {
+									comparison = 1
+								} else if (A < B) {
+									comparison = -1
+								}
+								return comparison
+							}
+							day.sort(compare)
 
-                return (
-                  <div onClick={clicking} className={`month-box  ${todaySelected} ${isGrayed} ${isSelected}`} key={i}>
-                    <span>{current.format('D')}</span>
-                    <div className='month-display' >
-                      {Array(day.length).fill(0).map((v, n) => (
-                        <div style={{display: 'block', width: '100%', height: '18px', position: 'relative'}}>
-                          <div style={{marginLeft: '6px', marginTop: '4px', float: 'left', width: '10px', height: '10px', backgroundColor: day[n].color, borderRadius: '10px'}}></div>
-                          <div style={{marginLeft: '6px', float: 'left', width: 'calc(100% - 71px)', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap', fontWeight: 600, color: 'black'}}>{day[n].title}</div>
-                          <div style={{lineHeight: '1.5', float: 'right', marginRight: '6px', fontSize: '14px', fontWeight: 600, color: 'gray'}}>{day[n].startHours}:{day[n].startMinutes}</div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                );
-              })
-            }
-          </div>,
-      );
-    }
-    return calendar;
-  };
+							return (
+								<div onClick={clicking} className={`month-box  ${todaySelected} ${isGrayed} ${isSelected}`} key={i}>
+									<span>{current.format('D')}</span>
+									<div className='month-display' >
+										{Array(day.length).fill(0).map((v, n) => (
+											<div style={{display: 'block', width: '100%', height: '18px', position: 'relative'}}>
+												<div style={{marginLeft: '6px', marginTop: '4px', float: 'left', width: '10px', height: '10px', backgroundColor: day[n].color, borderRadius: '10px'}}></div>
+												<div style={{marginLeft: '6px', float: 'left', width: 'calc(100% - 71px)', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap', fontWeight: 600, color: 'black'}}>{day[n].title}</div>
+												<div style={{lineHeight: '1.5', float: 'right', marginRight: '6px', fontSize: '14px', fontWeight: 600, color: 'gray'}}>{day[n].startHours}:{day[n].startMinutes}</div>
+											</div>
+										))}
+									</div>
+								</div>
+							)
+						})
+					}
+				</div>,
+			)
+		}
+		return calendar
+	}
 
-  return (
-    <div className='month-component'>
-      <div>
-        <span style={{color: 'black', fontSize: '30px', fontWeight: 500}}>{m.set('month', nowMonth).format('MMMM')}</span>
-        <span style={{fontSize: '36px', color: 'red'}}>{nowYear}</span>
-      </div>
-      <div className="month-row">
-        <div className="month-day">
-          <span>SUN</span>
-        </div>
-        <div className="month-day">
-          <span>MON</span>
-        </div>
-        <div className="month-day">
-          <span>TUE</span>
-        </div>
-        <div className="month-day">
-          <span>WED</span>
-        </div>
-        <div className="month-day">
-          <span>THU</span>
-        </div>
-        <div className="month-day">
-          <span>FRI</span>
-        </div>
-        <div className="month-day">
-          <span>SAT</span>
-        </div>
-      </div>
-      {Generate()}
-    </div>
-  );
-};
-export default Month;
+	return (
+		<div className='month-component'>
+			<div>
+				<span style={{color: 'black', fontSize: '30px', fontWeight: 500}}>{m.set('month', nowMonth).format('MMMM')}</span>
+				<span style={{fontSize: '36px', color: 'red'}}>{nowYear}</span>
+			</div>
+			<div className="month-row">
+				<div className="month-day">
+					<span>SUN</span>
+				</div>
+				<div className="month-day">
+					<span>MON</span>
+				</div>
+				<div className="month-day">
+					<span>TUE</span>
+				</div>
+				<div className="month-day">
+					<span>WED</span>
+				</div>
+				<div className="month-day">
+					<span>THU</span>
+				</div>
+				<div className="month-day">
+					<span>FRI</span>
+				</div>
+				<div className="month-day">
+					<span>SAT</span>
+				</div>
+			</div>
+			{Generate()}
+		</div>
+	)
+}
+export default Month
--- src/component/molcules/Week/Week.tsx
+++ src/component/molcules/Week/Week.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import moment from 'moment';
-import './style.css';
+import React, {useEffect, useState} from 'react'
+import moment from 'moment'
+import './style.css'
 
 interface IWeek {
     nowDay: number
@@ -11,133 +11,133 @@ interface IWeek {
 }
 
 const Week = ({
-  nowDay,
-  nowWeek,
-  monthStorage,
-  yearStorage,
-  storage,
+	nowDay,
+	nowWeek,
+	monthStorage,
+	yearStorage,
+	storage,
 }:IWeek) => {
-  const m = moment();
-  const [today, setToday] = useState(m.format('YYYYMMDD'));
-  const [time, setTime] = useState(m.format('LT'));
-  const [location, setLocation] = useState(m.hours() * 61 + 51 + m.minutes());
+	const m = moment()
+	const [today, setToday] = useState(m.format('YYYYMMDD'))
+	const [time, setTime] = useState(m.format('LT'))
+	const [location, setLocation] = useState(m.hours() * 61 + 51 + m.minutes())
 
-  const Generate = () => {
-    m.set('year', yearStorage);
-    m.set('month', monthStorage);
-    m.set('week', nowWeek);
-    const week = m.week();
-    return (
-      <>
-        {Array(7).fill(0).map((n, i) => {
-          const current = m.week(week).startOf('week').add(n + i, 'day');
-          const todaySelect = today === current.format('YYYYMMDD') ? 'week-selected' : '';
-          const isGrayed = Number(current.format('MM')) === Number(monthStorage) + 1 ? '' : 'week-grayed';
-          const sun = i === 0 ? 'SUN' : '';
-          const mon = i === 1 ? 'MON' : '';
-          const tue = i === 2 ? 'TUE' : '';
-          const wed = i === 3 ? 'WED' : '';
-          const thu = i === 4 ? 'THU' : '';
-          const fri = i === 5 ? 'FRI' : '';
-          const sat = i === 6 ? 'SAT' : '';
+	const Generate = () => {
+		m.set('year', yearStorage)
+		m.set('month', monthStorage)
+		m.set('week', nowWeek)
+		const week = m.week()
+		return (
+			<>
+				{Array(7).fill(0).map((n, i) => {
+					const current = m.week(week).startOf('week').add(n + i, 'day')
+					const todaySelect = today === current.format('YYYYMMDD') ? 'week-selected' : ''
+					const isGrayed = Number(current.format('MM')) === Number(monthStorage) + 1 ? '' : 'week-grayed'
+					const sun = i === 0 ? 'SUN' : ''
+					const mon = i === 1 ? 'MON' : ''
+					const tue = i === 2 ? 'TUE' : ''
+					const wed = i === 3 ? 'WED' : ''
+					const thu = i === 4 ? 'THU' : ''
+					const fri = i === 5 ? 'FRI' : ''
+					const sat = i === 6 ? 'SAT' : ''
 
-          const day: any = [];
-          {
-            Array(storage.length).forEach((v, n) => {
-              if (storage[n].year === Number(current.year())) {
-                if (storage[n].month === Number(current.month() + 1)) {
-                  if (storage[n].day === Number(current.date())) {
-                    day.push(storage[n]);
-                  }
-                }
-              }
-            });
-          }
+					const day: any = []
+					{
+						Array(storage.length).forEach((v, n) => {
+							if (storage[n].year === Number(current.year())) {
+								if (storage[n].month === Number(current.month() + 1)) {
+									if (storage[n].day === Number(current.date())) {
+										day.push(storage[n])
+									}
+								}
+							}
+						})
+					}
 
-          return (
-            <div style={{position: 'relative', width: 'calc((100vw - 430px) / 7)', float: 'left'}}>
-              <div style={{width: 'calc((100vw - 430px) / 7)', height: '50px'}}>
-                <div className="week-day">
-                  <span>{sun}{mon}{tue}{wed}{thu}{fri}{sat}</span>
-                </div>
-                <div className={`week-box  ${todaySelect} ${isGrayed} `} key={i}>
-                  <span>{current.format('D')}</span>
-                </div>
-              </div>
-              <div style={{width: '100%'}}>
-                {Array(24).fill(0).map(() => {
-                  return (
-                    <div style={{float: 'left', borderBottom: '1px solid #e9e9e9', height: '60px', width: '100%', borderLeft: '1px solid #e9e9e9'}}></div>
-                  );
-                })}
-              </div>
-              {Array(day.length).fill(0).map((v, n) => {
-                const height:number = (Number(day[n].endHours) - Number(day[n].startHours)) * 61 + Number(day[n].endMinutes) - Number(day[n].startMinutes);
-                const style = {
-                  position: 'absolute',
-                  top: Number(day[n].startHours) * 61 + Number(day[n].startMinutes) + 48.55,
-                  right: 0,
-                  width: '100%',
-                  height: height,
-                } as React.CSSProperties;
-                const back = {
-                  backgroundColor: day[n].color,
-                  opacity: '0.5',
-                  width: '100%',
-                  height: '100%',
-                  float: 'left',
-                } as React.CSSProperties;
-                return (
-                  <div style={style}>
-                    <div style={back}></div>
-                    <div style={{position: 'relative', top: -height, fontSize: '15px', fontWeight: 600, color: 'black'}}>
-                      <div style={{position: 'absolute', top: height, left: 0, backgroundColor: day[n].color, height: height, width: '10px'}}></div>
-                      <div style={{marginLeft: '10px'}}>{day[n].startHours}:{day[n].startMinutes}</div>
-                      <div style={{width: 'calc(100% - 10px)', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap', height: '17px', marginLeft: '10px'}}>{day[n].title}</div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-      </>
-    );
-  };
+					return (
+						<div style={{position: 'relative', width: 'calc((100vw - 430px) / 7)', float: 'left'}}>
+							<div style={{width: 'calc((100vw - 430px) / 7)', height: '50px'}}>
+								<div className="week-day">
+									<span>{sun}{mon}{tue}{wed}{thu}{fri}{sat}</span>
+								</div>
+								<div className={`week-box  ${todaySelect} ${isGrayed} `} key={i}>
+									<span>{current.format('D')}</span>
+								</div>
+							</div>
+							<div style={{width: '100%'}}>
+								{Array(24).fill(0).map(() => {
+									return (
+										<div style={{float: 'left', borderBottom: '1px solid #e9e9e9', height: '60px', width: '100%', borderLeft: '1px solid #e9e9e9'}}></div>
+									)
+								})}
+							</div>
+							{Array(day.length).fill(0).map((v, n) => {
+								const height:number = (Number(day[n].endHours) - Number(day[n].startHours)) * 61 + Number(day[n].endMinutes) - Number(day[n].startMinutes)
+								const style = {
+									position: 'absolute',
+									top: Number(day[n].startHours) * 61 + Number(day[n].startMinutes) + 48.55,
+									right: 0,
+									width: '100%',
+									height: height,
+								} as React.CSSProperties
+								const back = {
+									backgroundColor: day[n].color,
+									opacity: '0.5',
+									width: '100%',
+									height: '100%',
+									float: 'left',
+								} as React.CSSProperties
+								return (
+									<div style={style}>
+										<div style={back}></div>
+										<div style={{position: 'relative', top: -height, fontSize: '15px', fontWeight: 600, color: 'black'}}>
+											<div style={{position: 'absolute', top: height, left: 0, backgroundColor: day[n].color, height: height, width: '10px'}}></div>
+											<div style={{marginLeft: '10px'}}>{day[n].startHours}:{day[n].startMinutes}</div>
+											<div style={{width: 'calc(100% - 10px)', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap', height: '17px', marginLeft: '10px'}}>{day[n].title}</div>
+										</div>
+									</div>
+								)
+							})}
+						</div>
+					)
+				})}
+			</>
+		)
+	}
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setLocation(m.hours() * 61 + 51 + m.minutes());
-      setTime(m.format('LT'));
-    }, 5000);
-    return () => clearInterval(timer);
-  }, []);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setLocation(m.hours() * 61 + 51 + m.minutes())
+			setTime(m.format('LT'))
+		}, 5000)
+		return () => clearInterval(timer)
+	}, [])
 
-  return (
-    <div className='week-component'>
-      <div style={{float: 'left'}}>
-        <div className='week-left'>
-          <div style={{float: 'left', width: '70px', height: '55.55px', marginBottom: '45px', fontSize: '15px', fontWeight: 600}}>
-            <div style={{color: 'rgb(47, 72, 218)'}}>CW{m.set('week', nowWeek).format('W')}</div>
-            <div style={{color: 'black', fontSize: '14px'}}>{m.set('month', monthStorage).format('MMMM')}</div>
-            <div style={{color: 'red'}}>{yearStorage}</div>
-          </div>
-          <div>
-            {Array(24).fill(0).map((v, i) => {
-              return (
-                <div style={{color: 'gray', fontWeight: 600, textAlign: 'right', float: 'left', height: '61px', width: '100%'}}>
-                  {`${i + 1}:00`}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <Generate />
-      </div>
-      <div style={{borderBottom: '1px solid red', width: '100%', position: 'absolute', top: `${location}px`}}>
-        <div style={{fontWeight: 600, color: 'red', height: '0'}}>{time}</div>
-      </div>
-    </div>
-  );
-};
-export default Week;
+	return (
+		<div className='week-component'>
+			<div style={{float: 'left'}}>
+				<div className='week-left'>
+					<div style={{float: 'left', width: '70px', height: '55.55px', marginBottom: '45px', fontSize: '15px', fontWeight: 600}}>
+						<div style={{color: 'rgb(47, 72, 218)'}}>CW{m.set('week', nowWeek).format('W')}</div>
+						<div style={{color: 'black', fontSize: '14px'}}>{m.set('month', monthStorage).format('MMMM')}</div>
+						<div style={{color: 'red'}}>{yearStorage}</div>
+					</div>
+					<div>
+						{Array(24).fill(0).map((v, i) => {
+							return (
+								<div style={{color: 'gray', fontWeight: 600, textAlign: 'right', float: 'left', height: '61px', width: '100%'}}>
+									{`${i + 1}:00`}
+								</div>
+							)
+						})}
+					</div>
+				</div>
+				<Generate />
+			</div>
+			<div style={{borderBottom: '1px solid red', width: '100%', position: 'absolute', top: `${location}px`}}>
+				<div style={{fontWeight: 600, color: 'red', height: '0'}}>{time}</div>
+			</div>
+		</div>
+	)
+}
+export default Week
