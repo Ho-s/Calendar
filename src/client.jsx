f043d863eaad93a343dce4d6d8d92f4324497467
Ho-s <kjcoco13@gmail.com>
2021-09-07 10:20:49 +0900

    Add eslint related dependecies

--- src/client.jsx
+++ src/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-import Main from './pages/Main/Main.tsx'
+import Main from './pages/Main/Main.tsx';
 
-ReactDOM.render(<Main />, document.querySelector('#root'))
+ReactDOM.render(<Main />, document.querySelector('#root'));
--- src/component/organisms/Calendar/Calendar.tsx
+++ src/component/organisms/Calendar/Calendar.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import './style.css'
-import moment from 'moment'
+import React, {useEffect, useState} from 'react';
+import './style.css';
+import moment from 'moment';
 
 interface ICalendar {
     nowDay:number
@@ -19,118 +19,118 @@ interface ICalendar {
 }
 
 const Calendar = ({nowDay,
-    nowWeek,
-    nowMonth,
-    nowYear,
-    monthStorage,
-    yearStorage,
-    storage,
-    onClickLeft,
-    onClickRight,
-    onClickDay,
-    onClickWeek,
-    setMonth,
-    setYear
+  nowWeek,
+  nowMonth,
+  nowYear,
+  monthStorage,
+  yearStorage,
+  storage,
+  onClickLeft,
+  onClickRight,
+  onClickDay,
+  onClickWeek,
+  setMonth,
+  setYear,
 }:ICalendar) => {
-    const m = moment()
-    const Generate = () => {
-        m.set('year', nowYear)
-        m.set('month', nowMonth)
-        const startWeek = m.clone().startOf('month').week();
-        const endWeek = m.clone().endOf('month').week() === 1 ? 53 : m.clone().endOf('month').week();
-        const calendar = [];
-        for (let week = startWeek; week <= endWeek; week++) {
-            const thisWeekConst = Number(m.format('w')) === week ? 'today' : ''
-            const weekSelected = (Number(nowWeek) === week && Number(yearStorage) === nowYear && monthStorage === nowMonth) ? 'selected' : ''
-            calendar.push(
-                <div className='row' key={week}>
-                    <div onClick={onClickWeek} className={`cw ${thisWeekConst} ${weekSelected}`}>{week}</div>
-                    {
-                        Array(7).fill(0).map((n, i) => {
-                            let current = m.clone().week(week).startOf('week').add(n + i, 'day')
-                            let todaySelect = m.format('YYYYMMDD') === current.format('YYYYMMDD') ? 'today' : '';
-                            let isSelected = (Number(nowDay) === Number(current.format('D')) && Number(yearStorage) === nowYear && monthStorage === nowMonth) ? 'selected' : ''
-                            let clicking = current.format('MM') === m.format('MM') ? onClickDay : ((e: any) => e.preventDefault());
-                            let isGrayed = current.format('MM') === m.format('MM') ? '' : 'grayed';
+  const m = moment();
+  const Generate = () => {
+    m.set('year', nowYear);
+    m.set('month', nowMonth);
+    const startWeek = m.clone().startOf('month').week();
+    const endWeek = m.clone().endOf('month').week() === 1 ? 53 : m.clone().endOf('month').week();
+    const calendar = [];
+    for (let week = startWeek; week <= endWeek; week++) {
+      const thisWeekConst = Number(m.format('w')) === week ? 'today' : '';
+      const weekSelected = (Number(nowWeek) === week && Number(yearStorage) === nowYear && monthStorage === nowMonth) ? 'selected' : '';
+      calendar.push(
+          <div className='row' key={week}>
+            <div onClick={onClickWeek} className={`cw ${thisWeekConst} ${weekSelected}`}>{week}</div>
+            {
+              Array(7).fill(0).map((n, i) => {
+                const current = m.clone().week(week).startOf('week').add(n + i, 'day');
+                const todaySelect = m.format('YYYYMMDD') === current.format('YYYYMMDD') ? 'today' : '';
+                const isSelected = (Number(nowDay) === Number(current.format('D')) && Number(yearStorage) === nowYear && monthStorage === nowMonth) ? 'selected' : '';
+                const clicking = current.format('MM') === m.format('MM') ? onClickDay : ((e: any) => e.preventDefault());
+                const isGrayed = current.format('MM') === m.format('MM') ? '' : 'grayed';
 
-                            const day: any = []
-                            if (current.format('MM') === m.format('MM')) {
-                                Array(storage.length).forEach((v, n) => {
-                                    if (storage[n].year === Number(nowYear)) {
-                                        if (storage[n].month === Number(nowMonth + 1)) {
-                                            if (storage[n].day === current.date()) {
-                                                if (day.length < 3) {
-                                                    day.push(storage[n])
-                                                }
-                                            }
-                                        }
-                                    }
-                                })
-                            }
-
-                            return (
-                                <div onClick={clicking} className={`box ${todaySelect} ${isGrayed} ${isSelected}`} key={i}>
-                                    {current.format('D')}
-                                    <div style={{ position: 'absolute', top: '15px', width: '33px', height: '10px', pointerEvents: 'none' }}>
-                                        {Array(day.length).fill(0).map((v, n) => {
-                                            return (
-                                                <div style={{ marginLeft: '1px', marginRight: '1px', borderRadius: '6px', width: '6px', height: '6px', backgroundColor: day[n].color, display: 'inline-block' }}></div>
-                                            )
-                                        })}
-                                    </div>
-                                </div>
-                            )
-                        })
+                const day: any = [];
+                if (current.format('MM') === m.format('MM')) {
+                  Array(storage.length).forEach((v, n) => {
+                    if (storage[n].year === Number(nowYear)) {
+                      if (storage[n].month === Number(nowMonth + 1)) {
+                        if (storage[n].day === current.date()) {
+                          if (day.length < 3) {
+                            day.push(storage[n]);
+                          }
+                        }
+                      }
                     }
-                </div>
-            )
-        }
-        return calendar
+                  });
+                }
+
+                return (
+                  <div onClick={clicking} className={`box ${todaySelect} ${isGrayed} ${isSelected}`} key={i}>
+                    {current.format('D')}
+                    <div style={{position: 'absolute', top: '15px', width: '33px', height: '10px', pointerEvents: 'none'}}>
+                      {Array(day.length).fill(0).map((v, n) => {
+                        return (
+                          <div style={{marginLeft: '1px', marginRight: '1px', borderRadius: '6px', width: '6px', height: '6px', backgroundColor: day[n].color, display: 'inline-block'}}></div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })
+            }
+          </div>,
+      );
     }
+    return calendar;
+  };
 
-    useEffect(() => {
-        setYear(Number(m.set('year', nowYear).format('YYYY')))
-        setMonth(Number(m.set('month', nowMonth).format('MMMM')))
-    }, [])
+  useEffect(() => {
+    setYear(Number(m.set('year', nowYear).format('YYYY')));
+    setMonth(Number(m.set('month', nowMonth).format('MMMM')));
+  }, []);
 
-    return (
-        <div className="calendar">
-            <div className="body">
-                <span style={{ fontSize: '32px', fontWeight: 600 }} >{m.set('month', nowMonth).format('MMMM')}</span>
-                <span style={{ fontSize: '32px', color: 'red', fontWeight: 600 }}>{nowYear}</span>
-                <button style={{ marginLeft: '10px' }} onClick={onClickRight}>&gt;</button>
-                <button onClick={onClickLeft}>&lt;</button>
-            </div>
-            <div>
-                <div className="row">
-                    <div style={{ borderRight: '1px solid gray', color: 'gray' }} className="day">
-                        <span>CW</span>
-                    </div>
-                    <div className="day">
-                        <span>SUN</span>
-                    </div>
-                    <div className="day">
-                        <span>MON</span>
-                    </div>
-                    <div className="day">
-                        <span>TUE</span>
-                    </div>
-                    <div className="day">
-                        <span>WED</span>
-                    </div>
-                    <div className="day">
-                        <span>THU</span>
-                    </div>
-                    <div className="day">
-                        <span>FRI</span>
-                    </div>
-                    <div className="day">
-                        <span>SAT</span>
-                    </div>
-                </div>
-                {Generate()}
-            </div>
+  return (
+    <div className="calendar">
+      <div className="body">
+        <span style={{fontSize: '32px', fontWeight: 600}} >{m.set('month', nowMonth).format('MMMM')}</span>
+        <span style={{fontSize: '32px', color: 'red', fontWeight: 600}}>{nowYear}</span>
+        <button style={{marginLeft: '10px'}} onClick={onClickRight}>&gt;</button>
+        <button onClick={onClickLeft}>&lt;</button>
+      </div>
+      <div>
+        <div className="row">
+          <div style={{borderRight: '1px solid gray', color: 'gray'}} className="day">
+            <span>CW</span>
+          </div>
+          <div className="day">
+            <span>SUN</span>
+          </div>
+          <div className="day">
+            <span>MON</span>
+          </div>
+          <div className="day">
+            <span>TUE</span>
+          </div>
+          <div className="day">
+            <span>WED</span>
+          </div>
+          <div className="day">
+            <span>THU</span>
+          </div>
+          <div className="day">
+            <span>FRI</span>
+          </div>
+          <div className="day">
+            <span>SAT</span>
+          </div>
         </div>
-    )
+        {Generate()}
+      </div>
+    </div>
+  );
 };
-export default Calendar
+export default Calendar;
