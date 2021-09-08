9068821c603c7f4fe68d973da8c01f2952c9a627
Ho-s <kjcoco13@gmail.com>
2021-09-08 11:54:04 +0900

    Change file path

--- src/client.jsx
+++ src/client.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import GlobalThemeProvider from "style/GlobalThemeProvider";
+import GlobalThemeProvider from "./style/GlobalThemeProvider";
 import Main from "./pages/Main/Main.tsx";
 
 ReactDOM.render(
