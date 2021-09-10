fe921a989c62b5723921bc509fdd3a203c77c616
Ho-s <kjcoco13@gmail.com>
2021-09-10 11:25:56 +0900

    Change all inline style into styled component in Week

--- src/component/molcules/Week/Week.tsx
+++ src/component/molcules/Week/Week.tsx
@@ -36,21 +36,32 @@ const Week = ({ nowWeek, monthStorage, yearStorage, storage }: IWeek) => {
 							Number(current.format('MM')) === Number(monthStorage) + 1
 								? ''
 								: 'week-grayed'
-						const sun = i === 0 ? 'SUN' : ''
-						const mon = i === 1 ? 'MON' : ''
-						const tue = i === 2 ? 'TUE' : ''
-						const wed = i === 3 ? 'WED' : ''
-						const thu = i === 4 ? 'THU' : ''
-						const fri = i === 5 ? 'FRI' : ''
-						const sat = i === 6 ? 'SAT' : ''
+						const whatDay = () => {
+							switch (i) {
+								case 0:
+									return 'SUN'
+								case 1:
+									return 'MON'
+								case 2:
+									return 'TUE'
+								case 3:
+									return 'WED'
+								case 4:
+									return 'THU'
+								case 5:
+									return 'FRI'
+								case 6:
+									return 'SAT'
+							}
+						}
 
 						const day: any = []
 						{
-							Array(storage.length).forEach((v, n) => {
-								if (storage[n].year === Number(current.year())) {
-									if (storage[n].month === Number(current.month() + 1)) {
-										if (storage[n].day === Number(current.date())) {
-											day.push(storage[n])
+							storage.forEach((v: any) => {
+								if (v.year === Number(current.year())) {
+									if (v.month === Number(current.month() + 1)) {
+										if (v.day === Number(current.date())) {
+											day.push(v)
 										}
 									}
 								}
@@ -58,115 +69,47 @@ const Week = ({ nowWeek, monthStorage, yearStorage, storage }: IWeek) => {
 						}
 
 						return (
-							<div
-								style={{
-									position: 'relative',
-									width: 'calc((100vw - 430px) / 7)',
-									float: 'left',
-								}}
-							>
-								<div
-									style={{ width: 'calc((100vw - 430px) / 7)', height: '50px' }}
-								>
-									<S.WeekDay>
-										<span>
-											{sun}
-											{mon}
-											{tue}
-											{wed}
-											{thu}
-											{fri}
-											{sat}
-										</span>
-									</S.WeekDay>
+							<S.BodyWrapper>
+								<S.BodyTopWrapper>
+									<S.WeekDay>{whatDay()}</S.WeekDay>
 									<S.WeekBox className={`${todaySelect} ${isGrayed}`} key={i}>
-										<span>{current.format('D')}</span>
+										{current.format('D')}
 									</S.WeekBox>
-								</div>
-								<div style={{ width: '100%' }}>
-									{Array(24)
-										.fill(0)
-										.map(() => {
-											return (
-												<div
-													style={{
-														float: 'left',
-														borderBottom: '1px solid #e9e9e9',
-														height: '60px',
-														width: '100%',
-														borderLeft: '1px solid #e9e9e9',
-													}}
-												></div>
-											)
-										})}
-								</div>
-								{Array(day.length)
+								</S.BodyTopWrapper>
+								{Array(24)
 									.fill(0)
-									.map((v, n) => {
-										const height: number =
-											(Number(day[n].endHours) - Number(day[n].startHours)) *
-												61 +
-											Number(day[n].endMinutes) -
-											Number(day[n].startMinutes)
-										const style = {
-											position: 'absolute',
-											top:
-												Number(day[n].startHours) * 61 +
-												Number(day[n].startMinutes) +
-												48.55,
-											right: 0,
-											width: '100%',
-											height: height,
-										} as React.CSSProperties
-										const back = {
-											backgroundColor: day[n].color,
-											opacity: '0.5',
-											width: '100%',
-											height: '100%',
-											float: 'left',
-										} as React.CSSProperties
-										return (
-											<div style={style}>
-												<div style={back}></div>
-												<div
-													style={{
-														position: 'relative',
-														top: -height,
-														fontSize: '15px',
-														fontWeight: 600,
-														color: 'black',
-													}}
-												>
-													<div
-														style={{
-															position: 'absolute',
-															top: height,
-															left: 0,
-															backgroundColor: day[n].color,
-															height: height,
-															width: '10px',
-														}}
-													></div>
-													<div style={{ marginLeft: '10px' }}>
-														{day[n].startHours}:{day[n].startMinutes}
-													</div>
-													<div
-														style={{
-															width: 'calc(100% - 10px)',
-															textOverflow: 'ellipsis',
-															overflow: 'hidden',
-															whiteSpace: 'nowrap',
-															height: '17px',
-															marginLeft: '10px',
-														}}
-													>
-														{day[n].title}
-													</div>
-												</div>
-											</div>
-										)
+									.map(() => {
+										return <S.TimeLine />
 									})}
-							</div>
+								{day.map((v: any) => {
+									const height: number =
+										(Number(v.endHours) - Number(v.startHours)) * 61 +
+										Number(v.endMinutes) -
+										Number(v.startMinutes)
+									return (
+										<S.ScheduleWrapper
+											top={
+												Number(v.startHours) * 61 +
+												Number(v.startMinutes) +
+												48.55
+											}
+											height={height}
+										>
+											<S.ScheduleBack backgroundColor={v.color} />
+											<S.ScheduleBody top={-height}>
+												<S.ScheduleBodyBack
+													height={height}
+													backgroundColor={v.color}
+												/>
+												<S.ScheduleTime>
+													{v.startHours}:{v.startMinutes}
+												</S.ScheduleTime>
+												<S.ScheduleTitle>{v.title}</S.ScheduleTitle>
+											</S.ScheduleBody>
+										</S.ScheduleWrapper>
+									)
+								})}
+							</S.BodyWrapper>
 						)
 					})}
 			</>
@@ -183,59 +126,24 @@ const Week = ({ nowWeek, monthStorage, yearStorage, storage }: IWeek) => {
 
 	return (
 		<S.WeekComponent>
-			<div style={{ float: 'left' }}>
-				<S.WeekLeft>
-					<div
-						style={{
-							float: 'left',
-							width: '70px',
-							height: '55.55px',
-							marginBottom: '45px',
-							fontSize: '15px',
-							fontWeight: 600,
-						}}
-					>
-						<div style={{ color: 'rgb(47, 72, 218)' }}>
-							CW{m.set('week', nowWeek).format('W')}
-						</div>
-						<div style={{ color: 'black', fontSize: '14px' }}>
-							{m.set('month', monthStorage).format('MMMM')}
-						</div>
-						<div style={{ color: 'red' }}>{yearStorage}</div>
-					</div>
-					<div>
-						{Array(24)
-							.fill(0)
-							.map((v, i) => {
-								return (
-									<div
-										style={{
-											color: 'gray',
-											fontWeight: 600,
-											textAlign: 'right',
-											float: 'left',
-											height: '61px',
-											width: '100%',
-										}}
-									>
-										{`${i + 1}:00`}
-									</div>
-								)
-							})}
-					</div>
-				</S.WeekLeft>
-				<Generate />
-			</div>
-			<div
-				style={{
-					borderBottom: '1px solid red',
-					width: '100%',
-					position: 'absolute',
-					top: `${location}px`,
-				}}
-			>
-				<div style={{ fontWeight: 600, color: 'red', height: '0' }}>{time}</div>
-			</div>
+			<S.WeekLeft>
+				<S.WeekTitleWrapper>
+					<S.WeekTitleWeek>
+						CW{m.set('week', nowWeek).format('W')}
+					</S.WeekTitleWeek>
+					<S.WeekTitleMonth>
+						{m.set('month', monthStorage).format('MMMM')}
+					</S.WeekTitleMonth>
+					<S.WeekTitleYear>{yearStorage}</S.WeekTitleYear>
+				</S.WeekTitleWrapper>
+				{Array(24)
+					.fill(0)
+					.map((v, i) => {
+						return <S.Time>{`${i + 1}:00`}</S.Time>
+					})}
+			</S.WeekLeft>
+			<Generate />
+			<S.RedLine top={`${location}px`}>{time}</S.RedLine>
 		</S.WeekComponent>
 	)
 }
--- src/component/molcules/Week/style.ts
+++ src/component/molcules/Week/style.ts
@@ -39,6 +39,129 @@ export const WeekDay = styled.div`
     }
 `
 
+interface RedLineProps {
+    top:string
+}
 
+export const RedLine = styled.div<RedLineProps>`
+    font-weight: 600;
+    color: red;
+    height: 0;
+    border-bottom: 1px solid red;
+    width: 100%;
+    position: absolute;
+    top:${(props)=>props.top};
+`
 
+export const Time = styled.div`
+color: gray;
+font-weight: 600;
+text-align: right;
+float: left;
+height: 61px;
+width: 100%;
+`
 
+export const WeekTitleWrapper= styled.div`
+float: left;
+width: 70px;
+height: 55.55px;
+margin-bottom: 45px;
+font-size: 15px;
+font-weight: 600;
+`
+
+export const WeekTitleWeek = styled.div`
+color: rgb(47, 72, 218);
+`
+
+export const WeekTitleMonth = styled.div`
+color: black;
+ font-size: 14px;
+`
+export const WeekTitleYear = styled.div`
+color: 'red';
+`
+
+export const BodyWrapper = styled.div`
+position: relative;
+width: calc((100vw - 430px) / 7);
+float: left;
+`
+
+export const BodyTopWrapper = styled.div`
+width: calc((100vw - 430px) / 7);
+ height: 50px;
+`
+
+export const TimeLine = styled.div`
+    float: left;
+    border-bottom: 1px solid #e9e9e9;
+    height: 60px;
+    width: 100%;
+    border-left: 1px solid #e9e9e9;
+`
+
+interface ScheduleWrapperProps{
+    top:number
+    height:number
+}
+
+export const ScheduleWrapper = styled.div<ScheduleWrapperProps>`
+    right: 0;
+    position: absolute;
+    width: 100%;
+    top:${(props)=>props.top};
+    height:${(props)=>props.height};
+`
+
+interface ScheduleBackProps{
+    backgroundColor:string
+}
+
+export const ScheduleBack = styled.div<ScheduleBackProps>`
+opacity: 0.5;
+width: 100%;
+height: 100%;
+float: left;
+background-color: ${(props)=>props.backgroundColor};
+`
+
+interface ScheduleBodyProps{
+    top:number
+}
+
+export const ScheduleBody = styled.div<ScheduleBodyProps>`
+font-size: 15px;
+font-weight: 600;
+color: black;
+position: relative;
+top:${(props)=>props.top};
+`
+
+interface ScheduleBodyBackProps{
+    height:number
+    backgroundColor:number
+}
+
+export const ScheduleBodyBack = styled.div<ScheduleBodyBackProps>`
+    width: 10px;
+    position: absolute;
+    left: 0;
+    top:${(props)=>props.height};
+    height:${(props)=>props.height};
+    background-color:${(props)=>props.backgroundColor};
+`
+
+export const ScheduleTime = styled.div`
+margin-left: 10px;
+`
+
+export const ScheduleTitle = styled.div`
+width: calc(100% - 10px);
+text-overflow: ellipsis;
+overflow: hidden;
+white-space: nowrap;
+height: 17px;
+margin-left: 10px;
+`
