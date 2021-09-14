ece72e3e276aef5200971eb83af9dd4098f155df
Ho-s <kjcoco13@gmail.com>
2021-09-14 14:18:47 +0900

    Add Type file

---
+++ src/types/type.ts
@@ -0,0 +1,15 @@
+export interface StorageType {
+	name: string | number
+	title:string
+	year: number
+	month: number
+	day: number
+	week: number
+	startHours: number|string
+	startMinutes: number|string
+	endHours: number|string
+	endMinutes: number|string
+	color:string
+}
+
+export default StorageType
