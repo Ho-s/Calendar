35898ead03b34cdb990b7dc89e716ddc8f1a9173
Ho-s <kjcoco13@gmail.com>
2021-09-16 10:04:27 +0900

    Fix Error

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,15 +4,14 @@ import Summary from '../../component/organisms/Summary/Summary'
 import MainCalendar from '../../component/organisms/MainCalendar/MainCalendar'
 import AddScheduler from '../../component/organisms/AddScheduler/AddScheduler'
 import * as S from './style'
-import { storage, nowDay, nowWeek, setItemsInStorage } from '../../stores/store'
+import { storage, setItemsInStorage } from '../../stores/store'
 import { useReactiveVar } from '@apollo/client'
 
 const Main: React.FunctionComponent = () => {
 	const storageProps = useReactiveVar(storage)
-	const nowDayProps = useReactiveVar(nowDay)
-	const nowWeekProps = useReactiveVar(nowWeek)
+
 	useEffect(() => {
-		localStorage.setItem('storageProps', JSON.stringify(storageProps))
+		localStorage.setItem('storage', JSON.stringify(storageProps))
 	}, [storageProps])
 
 	useEffect(() => {
