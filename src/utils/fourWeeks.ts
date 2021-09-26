add6e18b552ca5394eb97cc1103178003360500b
Ho-s <kjcoco13@gmail.com>
2021-09-26 10:47:21 +0900

    Add fourWeeks in utils

---
+++ src/utils/fourWeeks.ts
@@ -0,0 +1,19 @@
+import moment from 'moment'
+
+const fourWeeks = (month:number, year:number) => {
+    const m = moment()
+    m.set('month',month)
+    m.set('year',year)
+    const startWeek = m.clone().startOf('month').week()
+    const endWeek =
+        m.clone().endOf('month').week() === 1
+            ? 53
+            : m.clone().endOf('month').week()
+    const calendar = Array(endWeek - startWeek + 1)
+        .fill(0)
+        .map((_, i) => startWeek + i)
+    return calendar
+}
+
+
+export default fourWeeks
