217fad1d8f0d92e7348ff72b2a8c5f07e233af8a
Ho-s <kjcoco13@gmail.com>
2021-09-28 09:22:33 +0900

    Add return type

--- src/utils/fourWeeks.ts
+++ src/utils/fourWeeks.ts
@@ -1,18 +1,18 @@
 import moment from 'moment'
 
-const fourWeeks = (month:number, year:number) => {
-    const m = moment()
-    m.set('month',month)
-    m.set('year',year)
-    const startWeek = m.clone().startOf('month').week()
-    const endWeek =
+const fourWeeks = (month:number, year:number):number[] => {
+	const m = moment()
+	m.set('month',month)
+	m.set('year',year)
+	const startWeek = m.clone().startOf('month').week()
+	const endWeek =
         m.clone().endOf('month').week() === 1
-            ? 53
-            : m.clone().endOf('month').week()
-    const calendar = Array(endWeek - startWeek + 1)
-        .fill(0)
-        .map((_, i) => startWeek + i)
-    return calendar
+        	? 53
+        	: m.clone().endOf('month').week()
+	const calendar = Array(endWeek - startWeek + 1)
+		.fill(0)
+		.map((_, i) => startWeek + i)
+	return calendar
 }
 
 
