4f3f44ae21696afcdd4560d961400beb93b35829
Ho-s <kjcoco13@gmail.com>
2021-10-01 13:35:07 +0900

    Add create schedule logic

--- client/src/pages/BaseTemplate/BaseTemplate.tsx
+++ client/src/pages/BaseTemplate/BaseTemplate.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
-import { useReactiveVar } from '@apollo/client'
+import { useMutation, useQuery, useReactiveVar } from '@apollo/client'
 
 import Header from '../../component/organisms/Header/Header'
 import Lefter from '../../component/organisms/Lefter/Lefter'
+import { CREATE_SCHEDULE } from '../../graphql/mutation'
 
 import { storage, setItemsInStorage } from '../../stores/store'
 
@@ -14,6 +15,26 @@ interface BaseTemplateProps {
 
 const BaseTemplate: React.FunctionComponent<BaseTemplateProps> = ({children}) => {
 	const storageProps = useReactiveVar(storage)
+	const [createSchedule]= useMutation(CREATE_SCHEDULE)
+
+	const onClick = async() => {
+		const input = {
+			title:'good',
+			year: 2013,
+			month: 9,
+			day: 13,
+			startHours: 9,
+			startMinutes: 23,
+			endHours: 24,
+			endMinutes: 25,
+			color:'#e9e9e9'
+		}
+		await createSchedule({
+			variables:{
+				input
+			}
+		})
+	}
 
 	useEffect(() => {
 		localStorage.setItem('storage', JSON.stringify(storageProps))
@@ -30,6 +51,7 @@ const BaseTemplate: React.FunctionComponent<BaseTemplateProps> = ({children}) =>
 			</S.MainLeft>
 			<S.MainRight>
 				<Header />
+				<div onClick={onClick}>hIWO</div>
 				{children}
 			</S.MainRight>
 		</S.Main>
