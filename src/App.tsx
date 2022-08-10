a74c4ce1836384864849b320587c4f423cddabd5
Joo-Byungho <kjcoco13@gmail.com>
2022-08-10 23:19:05 +0900

    Change browser router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import moment from 'moment'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import Day from './pages/Day/Day'
 import Week from './pages/Week/Week'
 import Month from './pages/Month/Month'
@@ -13,22 +13,34 @@ const App = () => {
 	const [location, setLocation] = useState<number>(m.hours() * 61 + m.minutes())
 	const [lineTime, setLineTime] = useState<string>(m.format('LT'))
 
-	useEffect(()=>{
-		const timer = setInterval(()=>{
+	useEffect(() => {
+		const timer = setInterval(() => {
 			setLineTime(m.format('LT') + '')
 			setLocation(m.hours() * 61 + m.minutes())
 		}, 1000)
 		return () => clearInterval(timer)
-	},[location])
+	}, [location])
 
-	return(
+	return (
 		<GlobalThemeProvider>
 			<Router>
 				<Switch>
-					<Route render={(props)=><Day {...props} location={location} lineTime={lineTime}/>} exact path='/'/>
-					<Route render={(props)=><Week {...props} location={location} lineTime={lineTime}/>} exact path='/week'/>
-					<Route exact path='/month' component={Month}/>
-					<Route exact path='/year' component={Year}/>
+					<Route
+						render={(props) => (
+							<Day {...props} location={location} lineTime={lineTime} />
+						)}
+						exact
+						path="/"
+					/>
+					<Route
+						render={(props) => (
+							<Week {...props} location={location} lineTime={lineTime} />
+						)}
+						exact
+						path="/week"
+					/>
+					<Route exact path="/month" component={Month} />
+					<Route exact path="/year" component={Year} />
 				</Switch>
 			</Router>
 		</GlobalThemeProvider>
